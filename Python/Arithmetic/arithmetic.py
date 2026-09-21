67b9722f6386cd01d46189ac46413f82a47e145a
shindeomkar0069 <shindeomkar0069@gmail.com>
2026-09-21 22:05:56 +0530

    Update arithmetic.py

--- Python/Arithmetic/arithmetic.py
+++ Python/Arithmetic/arithmetic.py
@@ -1,1 +1,16 @@
+def Addition(iNo1, iNo2):
+    return iNo1 + iNo2
 
+
+def Subtraction(iNo1, iNo2):
+    return iNo1 - iNo2
+
+
+def Multiplication(iNo1, iNo2):
+    return iNo1 * iNo2
+
+
+def Division(iNo1, iNo2):
+    if iNo2 == 0:
+        return 0
+    return iNo1 / iNo2
